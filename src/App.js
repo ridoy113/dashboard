d507a7871f2ebcb32f4e7238aa3fecbe3ee3357f
ridoy113 <almamounhridoy113@gmail.com>
2022-08-09 18:00:14 +0600

    make components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+
+
 
 
 const App = () => {
   return (
     <div>
-      <h1 className='underline text-3xl'>app</h1>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type='button'
+                className='text-3xl p-3 
+                hover:drop-shadow-xl 
+                hover: bg-light-gray text-white'
+                style={{ background: 'blue', 
+                borderRadius: "50%" }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        {activeMenu? (
+          <div>
+            Sidebar
+          </div>
+        ):(
+          <div>
+            Sidebar w-0
+          </div>
+        )}
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
