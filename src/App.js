43e4977e957b51ecff62d66dfc82f9e50ab53e17
ridoy113 <almamounhridoy113@gmail.com>
2022-08-10 18:19:52 +0600

    error fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import {Navbar, Footer, Sidebar, ThemeSettings} from './components'
+import{Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor}from './pages'
+
+
+
 
 
 
