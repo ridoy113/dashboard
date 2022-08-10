4756803b014adddee620f644cdd39b23808fd862
ridoy113 <almamounhridoy113@gmail.com>
2022-08-10 17:24:10 +0600

    make all route

--- src/App.js
+++ src/App.js
@@ -53,8 +53,34 @@ const App = () => {
 
           <div>
             <Routes>
+              {/* Dashboard */}
               <Route path='/' element="ECommerce" />
               <Route path='/ecommerce' element="ECommerce" />
+
+
+              {/* Pages */}
+              <Route path='/orders' element="orders" />
+              <Route path='/employees' element="Employees" />
+              <Route path='/customers' element="Customers" />
+
+
+              {/* Apps */}
+              <Route path='/kanban' element="Kanban" />
+              <Route path='/editor' element="Editor" />
+              <Route path='/calender' element="Calender" />
+              <Route path='/color-picker' element="ColorPicker" />
+
+
+              {/* Charts */}
+              <Route path='/line' element="Line" />
+              <Route path='/area' element="Area" />
+              <Route path='/bar' element="Bar" />
+              <Route path='/pie' element="Pie" />
+              <Route path='/financial' element="Financial" />
+              <Route path='/color-mapping' element="ColorMapping" />
+              <Route path='/pyramid' element="Pyramid" />
+              <Route path='/stacked' element="Stacked" />
+
             </Routes>
           </div>
 
