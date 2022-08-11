891a67c6763abd6fcf945b89d5d794501681900a
ridoy113 <almamounhridoy113@gmail.com>
2022-08-11 15:33:39 +0600

    complite full side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,6 +10,9 @@ import { links } from '../data/dummy'
 const Sidebar = () => {
   const activeMenu = true;
 
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+
 
   return (
     <div className='ml-3 h-screen
@@ -25,10 +28,41 @@ const Sidebar = () => {
             <SiShopware /><span>Shoppy</span>
           </Link>
           <TooltipComponent content="Menu" position='BottomCenter'>
-            <button>
+            <button type='button'
+              onClick={() => { }}
+              className="text-xl rounded-full 
+            p-3 hover:bg-light-gray mt-4
+            md:hidden"
+            >
               <MdOutlineCancel />
             </button>
           </TooltipComponent>
+        </div>
+        <div className='mt-10'>
+          {
+            links.map((item) => (
+              <div key={item.title}>
+                <p className='text-gray-400 m-3 
+                    mt-4 uppercase'>
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={() => { }}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink}
+                  >
+                    {link.icon}
+                    <span className='capitalize'>
+                      {link.name}
+                    </span>
+                  </NavLink>
+                ))}
+              </div>
+            ))
+          }
         </div>
       </>)}
     </div>
