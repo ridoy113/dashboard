3c2f4ce71285dc69e4982838ff558773d64067a9
ridoy113 <almamounhridoy113@gmail.com>
2022-08-15 21:25:19 +0600

    import all data in ecommarce

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs'
+import { GoPrimitiveDot } from 'react-icons/go'
+
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+
+
+
 
 const Ecommerce = () => {
   return (
-    <div></div>
+    <div>Ecommerce</div>
   )
 }
 
