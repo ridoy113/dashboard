85672aef7730964d6da07cacceb542d23be12020
ridoy113 <almamounhridoy113@gmail.com>
2022-08-18 15:32:59 +0600

    make start ecommarce

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { BsCurrencyDollar } from 'react-icons/bs'
-import { GoPrimitiveDot } from 'react-icons/go'
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
-import { Stacked, Pie, Button, SparkLine } from '../components';
-import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
+import product9 from '../data/product9.jpg';
 
 
 
 
 const Ecommerce = () => {
   return (
-    <div>
+    <div className='mt-12'>
+      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
 
+        </div>
+      </div>
     </div>
   )
 }
