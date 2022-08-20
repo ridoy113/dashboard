9ed3650faae56f70a2a1156b03a7dec6ffe743ee
ridoy113 <almamounhridoy113@gmail.com>
2022-08-20 16:44:32 +0600

    make expanse

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -74,7 +74,19 @@ const Ecommerce = () => {
         <div className='bg-white dark:text-gray-200 
         dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
           <div className='flex justify-between'>
-            <p>Revenue Updates</p>
+            <p className='font-semibold text-xl'>Revenue Updates</p>
+            <div className='flex items-center gap-4'>
+              <p className='flex items-center gap-2 text-gray-600 
+              hover:drop-shadow-xl'>
+                <span><GoPrimitiveDot /></span>
+                <span>Expanse</span>
+              </p>
+              <p className='flex items-center gap-2 text-green-400 
+              hover:drop-shadow-xl'>
+                <span><GoPrimitiveDot /></span>
+                <span>Budget</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
