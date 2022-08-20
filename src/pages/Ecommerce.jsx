d35577f33035d2ff899dcdf74f6d4bb516e16c2e
ridoy113 <almamounhridoy113@gmail.com>
2022-08-20 17:56:29 +0600

    sparkline make complite

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -110,17 +110,25 @@ const Ecommerce = () => {
 
 
               <div className='mt-5'>
-                <SparkLine 
-                currentColor="blue"
-                id="line-sparkline"
-                type="Line"
-                height="80px"
-                width="250px"
-                data={SparklineAreaData}
-                color="blue"
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
                 />
               </div>
 
+              <div className='mt-10'>
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
 
             </div>
           </div>
