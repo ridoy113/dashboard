8a2c073522a6aaa8d9a393e675b0faf3166d9d15
ridoy113 <almamounhridoy113@gmail.com>
2022-08-20 16:37:45 +0600

    add ravenu

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -69,6 +69,16 @@ const Ecommerce = () => {
         </div>
 
       </div>
+
+      <div className='flex gap-10 flex-wrap justify-center'>
+        <div className='bg-white dark:text-gray-200 
+        dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+          <div className='flex justify-between'>
+            <p>Revenue Updates</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
