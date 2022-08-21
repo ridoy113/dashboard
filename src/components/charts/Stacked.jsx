21c7c584570ea454cca5e93a407e85a1e5187edd
ridoy113 <almamounhridoy113@gmail.com>
2022-08-21 12:04:48 +0600

    stacked complite

--- src/components/charts/Stacked.jsx
+++ src/components/charts/Stacked.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import { ChartComponent, SeriesCollectionDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts'
+import { ChartComponent, SeriesCollectionDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip, SeriesDirective } from '@syncfusion/ej2-react-charts';
+
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
 
 
 
-const Stacked = () => {
+const Stacked = ({ width, height }) => {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+
+    </ChartComponent>
   )
 }
 
