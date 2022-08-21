97ea6763c5fa6da23a329c1a26945c77513888c2
ridoy113 <almamounhridoy113@gmail.com>
2022-08-21 15:22:41 +0600

    input data on order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { GridComponent, ColumnsDirective, CoumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
-import { ordersData, contextMenuItems, orderGrid } from '../data/dummy';
-import{Header} from '../components';
+import { ordersData, contextMenuItems, orderGrid, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+import { ColumnDirective } from '@syncfusion/ej2-react-charts';
 
 
 
 
 const Orders = () => {
   return (
-    <div>
-      <Header category="Page" title="Orders"/>
+    <div className='m-2 ms:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   )
 }
