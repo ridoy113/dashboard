5db5ab8402354ce2909050f3b8efced6af7f17e6
ridoy113 <almamounhridoy113@gmail.com>
2022-08-21 15:56:13 +0600

    make customrs page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Section, Inject, Edit, Tolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
+
+
+
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 ms:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page]} />
+      </GridComponent>
+    </div>
   )
 }
 
